e778d963692f46afeafc44cef132f969fab2bb90
luisherreralg <lherreralg@outlook.es>
2022-11-12 10:59:00 +0100

    :building_construction: Added the products.repository.ts
    Created a standar service for the products

---
+++ src/infrastructure/services/products.repository.ts
@@ -0,0 +1,57 @@
+import { Product } from '../types/types';
+import { Repository } from './repository';
+
+export class ProductRepository implements Repository<Product> {
+    url: string;
+    constructor(url = '') {
+        this.url = url ? url : (process.env.REACT_APP_URL_PRODUCTS as string);
+    }
+
+    createError(response: Response) {
+        const message = `Error ${response.status}: ${response.statusText}`;
+        const error = new Error(message);
+        error.name = 'HTTPError';
+        return error;
+    }
+
+    getAll(): Promise<Array<Product>> {
+        return fetch(this.url).then((response) => {
+            if (response.ok) return response.json();
+            throw this.createError(response);
+        });
+    }
+
+    create(task: Partial<Product>): Promise<Product> {
+        return fetch(this.url, {
+            method: 'POST',
+            body: JSON.stringify(task),
+            headers: {
+                'content-type': 'application/json',
+            },
+        }).then((response) => {
+            if (response.ok) return response.json();
+            throw this.createError(response);
+        });
+    }
+
+    delete(id: string): Promise<void> {
+        return fetch(`${this.url}/${id}`, {
+            method: 'DELETE',
+        }).then((response) => {
+            if (!response.ok) throw this.createError(response);
+        });
+    }
+
+    update(partialProduct: Partial<Product>): Promise<Product> {
+        return fetch(`${this.url}/${partialProduct.id}`, {
+            method: 'PATCH',
+            body: JSON.stringify(partialProduct),
+            headers: {
+                'content-type': 'application/json',
+            },
+        }).then((response) => {
+            if (response.ok) return response.json();
+            throw this.createError(response);
+        });
+    }
+}
