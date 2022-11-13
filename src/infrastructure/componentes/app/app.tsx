218e8651dcbd3a2b22219039e67291863c29dbd3
luisherreralg <lherreralg@outlook.es>
2022-11-13 16:28:41 +0100

    :white_check_mark: Updated app test to add the Header component

--- src/infrastructure/componentes/app/app.tsx
+++ src/infrastructure/componentes/app/app.tsx
@@ -1,9 +1,11 @@
+import Header from '../header/header';
 import { AppRoutes } from '../routes/app.routes';
 import './app.css';
 
 export function App() {
     return (
         <>
+            <Header></Header>
             <AppRoutes></AppRoutes>
         </>
     );
