02439725aa0ce91d3ae2bf92947bc922b8a90a66
luisherreralg <lherreralg@outlook.es>
2022-11-13 18:48:19 +0100

    :white_check_mark: Updated the product.list.item
    Like i added the conditional render, i had to update the test to get 100% coverage

--- src/infrastructure/componentes/product.list.item/product.list.item.test.tsx
+++ src/infrastructure/componentes/product.list.item/product.list.item.test.tsx
@@ -13,7 +13,7 @@ describe('When we render the ProductListItem', () => {
             description: [],
             image: 'image',
             onsale: true,
-            onsalePrice: 1,
+            onsalePrice: 10,
             price: 1,
         };
         render(
@@ -32,5 +32,30 @@ describe('When we render the ProductListItem', () => {
         const element = screen.getByAltText(/product/i);
         userEvent.click(element);
         expect(window.scrollTo).toHaveBeenCalledWith(0, 0);
+    });
+
+    test('When the product is on sale, it should display the onsale price', () => {
+        const element = screen.getByText(/10€/i);
+        expect(element).toBeInTheDocument();
+    });
+
+    test('When the product is not on sale, it should display the price', () => {
+        const mockProduct: Product = {
+            id: 'id',
+            section: 'section',
+            name: 'name',
+            description: [],
+            image: 'image',
+            onsale: false,
+            onsalePrice: 10,
+            price: 20,
+        };
+        render(
+            <Router>
+                <ProductListItem item={mockProduct} />
+            </Router>
+        );
+        const element = screen.getByText('20€');
+        expect(element).toBeInTheDocument();
     });
 });
