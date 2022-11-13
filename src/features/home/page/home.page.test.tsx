a96685161e205e183386ced78d014b1034502138
luisherreralg <lherreralg@outlook.es>
2022-11-13 12:30:15 +0100

    :white_check_mark: Added store provider to the tests
    Now i added some store info to the home page component, so i need to add the provider to the test to be all okey

--- src/features/home/page/home.page.test.tsx
+++ src/features/home/page/home.page.test.tsx
@@ -1,10 +1,20 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { appStore } from '../../../infrastructure/store/store';
+import { MemoryRouter as Router } from 'react-router-dom';
+
 import HomePage from './home.page';
 
 describe('Given HomePage component', () => {
     describe('When it is rendered', () => {
         test('Then it should display a title', () => {
-            render(<HomePage />);
+            render(
+                <Router>
+                    <Provider store={appStore}>
+                        <HomePage />
+                    </Provider>
+                </Router>
+            );
             const title = screen.getByText(/Home Page/i);
             expect(title).toBeInTheDocument();
         });
